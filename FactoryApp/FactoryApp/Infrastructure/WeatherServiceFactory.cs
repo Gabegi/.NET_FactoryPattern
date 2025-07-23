f37fa4a3d98c4007a82fdbaada20cdd60a666373
Gabegi <gabriel_pirastru@hotmail.fr>
2025-07-23 23:58:47 +0500

    Infra

---
+++ FactoryApp/FactoryApp/Infrastructure/WeatherServiceFactory.cs
@@ -0,0 +1,17 @@
+﻿using WeatherApp.Application.Interfaces;
+
+namespace WeatherApp.Infrastructure;
+
+public static class WeatherServiceFactory
+{
+    public static IWeatherService Create(string provider)
+    {
+        var httpClient = new HttpClient();
+
+        return provider.ToLower() switch
+        {
+            "openmeteo" => new OpenMeteoService(httpClient),
+            _ => throw new NotSupportedException($"Weather provider '{provider}' is not supported.")
+        };
+    }
+}
