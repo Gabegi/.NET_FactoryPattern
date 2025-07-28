1949400c9dbc7276a6e2fa0dd861968f427a6b57
Gabegi <gabriel_pirastru@hotmail.fr>
2025-07-28 12:40:29 +0500

    abstract factories

---
+++ FactoryApp/FactoryApp/Example/AbstractFactory.cs
@@ -0,0 +1,39 @@
+﻿namespace FactoryApp.Example
+{
+    public class AbstractFactory
+    {
+        public interface IHotdrink
+        {
+            void consume();
+        }
+
+        internal class Tea : IHotdrink
+        {
+            public void consume() { }
+        }
+
+        public interface IHotdrinkFactory
+        {
+            IHotdrink createHotdrink();
+        }
+        
+        internal class TeaFactory: IHotdrinkFactory
+        {
+            public IHotdrink createHotdrink()
+            {
+                return new Tea();
+            }
+        }
+
+        public class HotdrinkMachine
+        {
+            public enum AvailableDrink
+            {
+                Coffe, Tea
+            }
+
+            private Dictionary<AvailableDrink, IHotdrinkFactory> factories = 
+                new Dictionary<AvailableDrink, IHotdrinkFactory>();
+        }
+    }
+}
