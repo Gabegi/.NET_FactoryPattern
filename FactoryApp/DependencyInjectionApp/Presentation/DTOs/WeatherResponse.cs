a98bedd11b8b1e51baef4d5c74222c836d0d5176
Gabegi <gabriel_pirastru@hotmail.fr>
2025-07-30 10:56:13 +0500

    DI app

--- FactoryApp/DependencyInjectionApp/Presentation/DTOs/WeatherResponse.cs
+++ FactoryApp/DependencyInjectionApp/Presentation/DTOs/WeatherResponse.cs
@@ -2,13 +2,34 @@ namespace DependencyInjectionApp.Presentation.DTOs;
 
 public class WeatherResponse
 {
-    public double Temperature { get; set; }
-    public double WindSpeed { get; set; }
-    public string Units { get; set; }
-    public DateTime Timestamp { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double Generationtime_ms { get; set; }
+    public int Utc_offset_seconds { get; set; }
+    public string Timezone { get; set; } = string.Empty;
+    public string Timezone_abbreviation { get; set; } = string.Empty;
+    public double Elevation { get; set; }
+    public CurrentWeatherUnits Current_weather_units { get; set; } = new();
+    public CurrentWeather Current_weather { get; set; } = new();
+}
+public class CurrentWeatherUnits
+{
+    public string Time { get; set; } = string.Empty;
+    public string Interval { get; set; } = string.Empty;
+    public string Temperature { get; set; } = string.Empty;
+    public string Windspeed { get; set; } = string.Empty;
+    public string Winddirection { get; set; } = string.Empty;
+    public string Is_day { get; set; } = string.Empty;
+    public string Weathercode { get; set; } = string.Empty;
+}
 
-    // Domain logic
-    public bool IsCold => Temperature < 10;
-    public bool IsWarm => Temperature >= 20;
-    public bool IsWindy => WindSpeed > 20;
-} 
+public class CurrentWeather
+{
+    public string Time { get; set; } = string.Empty;
+    public int Interval { get; set; }
+    public double Temperature { get; set; }
+    public double Windspeed { get; set; }
+    public int Winddirection { get; set; }
+    public int Is_day { get; set; }
+    public int Weathercode { get; set; }
+}
