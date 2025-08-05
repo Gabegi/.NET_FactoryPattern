8bf6af63409d5b7081f52373066d49c30d6a4179
Gabegi <gabriel_pirastru@hotmail.fr>
2025-08-05 23:54:22 +0500

    keyed

--- FactoryApp/KeyedDependencyInjectionApp/Program.cs
+++ FactoryApp/KeyedDependencyInjectionApp/Program.cs
@@ -1,41 +1,35 @@
+using KeyedDependencyInjectionApp.Application.UseCases;
+using KeyedDependencyInjectionApp.Infrastructure;
+using KeyedDependencyInjectionApp.Infrastructure.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Register services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// Register HttpClient with configuration from appsettings
+builder.Services.AddHttpClient("WeatherApi", (serviceProvider, client) =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var timeoutSeconds = configuration.GetValue<int>("WeatherApi:TimeoutSeconds", 30);
+    var userAgent = configuration.GetValue<string>("WeatherApi:UserAgent", "WeatherApp/1.0");
+
+    client.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+    client.DefaultRequestHeaders.Add("User-Agent", userAgent);
+});
+
+// Keyed dependency injection - register services with keys
+builder.Services.AddKeyedScoped<IWeatherService, MockWeatherService>("mock");
+builder.Services.AddKeyedScoped<IWeatherService, OpenMeteoService>("openmeteo");
+
+// Register the use case that will receive the keyed service
+builder.Services.AddScoped<GetCurrentWeatherUseCase>();
+builder.Services.AddLogging();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
