e420409e9cb2e5705042782caa2f15142ec76456
Gabegi <gabriel_pirastru@hotmail.fr>
2025-08-28 10:47:33 +0800

    using httpmessage handler logger

--- FactoryApp/FactoryApp/Infrastructure/Configuration/ResilienceConfigurator.cs
+++ FactoryApp/FactoryApp/Infrastructure/Configuration/ResilienceConfigurator.cs
@@ -9,17 +9,7 @@ namespace FactoryApp.Infrastructure.Configuration
         {
             if (!request.EnableResilience) return;
 
-            clientBuilder.AddStandardResilienceHandler(options =>
-            {
-                options.TotalRequestTimeout.Timeout = TimeSpan.FromSeconds(60);
-                options.Retry.MaxRetryAttempts = 5;
-                options.Retry.Delay = TimeSpan.Zero;
-                options.CircuitBreaker.SamplingDuration = TimeSpan.FromSeconds(5);
-                options.CircuitBreaker.MinimumThroughput = 5;
-                options.CircuitBreaker.FailureRatio = 0.9;
-                options.CircuitBreaker.BreakDuration = TimeSpan.FromSeconds(5);
-                options.AttemptTimeout.Timeout = TimeSpan.FromSeconds(1);
-            });
+            
         }
     }
 }
