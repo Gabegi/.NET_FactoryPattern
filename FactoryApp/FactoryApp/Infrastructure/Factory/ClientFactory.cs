3ff80eb043382843f6deffbcec895598395835e8
Gabegi <gabriel_pirastru@hotmail.fr>
2025-09-01 09:38:24 +0800

    refactoring factory

--- FactoryApp/FactoryApp/Infrastructure/Factory/ClientFactory.cs
+++ FactoryApp/FactoryApp/Infrastructure/Factory/ClientFactory.cs
@@ -25,43 +25,5 @@ namespace FactoryApp.Infrastructure.Patterns
         {
             return _httpClientFactory.CreateClient(request.ClientName);
         }
-
-        //public HttpClient Create(WeatherRequest request)
-        //{
-        //    var clientName = $"{request.ServiceName}_client";
-
-        //    // Start with base client configuration
-
-        //    var httpClient = _httpClientFactory.CreateClient(clientName);
-        //    _baseClient.ConfigureBaseClient(httpClient, request);
-
-        //    if (request.EnableLogging is true)
-        //    {
-        //        _clientBuilder.AddHttpMessageHandler<LoggingHandler>();
-        //    }
-
-        //    if (request.EnableCaching is true)
-        //    {
-        //        _clientBuilder.AddHttpMessageHandler<CachingHandler>();
-        //    }
-
-        //    if (request.EnableResilience)
-        //    {
-        //        _clientBuilder.AddStandardResilienceHandler(options =>
-        //        {
-        //            options.TotalRequestTimeout.Timeout = TimeSpan.FromSeconds(60);
-        //            options.Retry.MaxRetryAttempts = 5;
-        //            options.Retry.Delay = TimeSpan.Zero;
-        //            options.CircuitBreaker.SamplingDuration = TimeSpan.FromSeconds(5);
-        //            options.CircuitBreaker.MinimumThroughput = 5;
-        //            options.CircuitBreaker.FailureRatio = 0.9;
-        //            options.CircuitBreaker.BreakDuration = TimeSpan.FromSeconds(5);
-        //            options.AttemptTimeout.Timeout = TimeSpan.FromSeconds(1);
-        //        });
-        //    }
-
-        //    // Use the injected factory to create the client
-        //    return _httpClientFactory.CreateClient(clientName);
-        //}
     }
 }
